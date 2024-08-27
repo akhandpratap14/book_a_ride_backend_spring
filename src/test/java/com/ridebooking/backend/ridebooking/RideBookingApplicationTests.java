f39c69619c45c536e2ae1459a80ed37cca78ba0c
Akhand Pratap Rai <akhandpratap892@gmail.com>
2024-08-27 13:26:27 +0530

    ride booking backend initial commit

---
+++ src/test/java/com/ridebooking/backend/ridebooking/RideBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridebooking.backend.ridebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
